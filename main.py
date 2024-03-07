b16d238a69af05408b0414ff49d4dec08ff41296
yuvalprogramer@gmail.com <yuvalprogramer@gmail.com>
2024-03-07 13:29:58 +0200

    upgrade model

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from tools import tools
 
 file = os.path.join("data", "ai-description.txt")
 context = utils.readfile(file)
-prefix_messages = [ChatMessage(role="system", content=context)]
 
 memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
 
@@ -22,7 +21,7 @@ memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
 def main():
     openai.api_key = os.environ.get("KEY")
     llm = OpenAI(model="gpt-3.5-turbo", openai_api_key=openai.api_key)
-    agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, prefix_messages=prefix_messages, memory=memory)
+    agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context, memory=memory)
 
     while (prompts := input("Enter a prompt (q to quit): ")) != "q":
         try:
